e88677323ac2870aef60cbf2a12b2d38c69c8aea
CesarVazquezTellez <cesarvazquezt00@gmail.com>
2025-07-03 11:52:33 -0600

    Adding Menu Bar

--- src/App.java
+++ src/App.java
@@ -1,10 +1,15 @@
 import javax.swing.JFrame;
+import screens.VentanaPrincipal;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        JFrame frame = new JFrame("App Swing");
-        frame.setSize(500, 500);
-        
-        frame.setVisible(true);
+        // JFrame frame = new JFrame("App Swing");
+        // frame.setSize(500, 500);
+        // frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // frame.setVisible(true);
+        VentanaPrincipal ventanaPrincipal = new VentanaPrincipal();
+        ventanaPrincipal.setSize(500, 500);
+        ventanaPrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventanaPrincipal.setVisible(true);
     }
 }
